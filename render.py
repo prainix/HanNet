4d70d4a51184ae427435ed09e510bfb2631e3203
Runjie Zhang <runjiehoos@gmail.com>
2017-03-24 07:34:44 -0700

    Add toy CNN model

--- render.py
+++ render.py
@@ -1,23 +1,23 @@
 # -*- coding: utf-8 -*-
 from PIL import Image, ImageFont, ImageDraw
-from scipy.misc import imread, imsave, imresize
+#from scipy.misc import imread, imsave, imresize
 import numpy as np
 
-fontpath = "./STXINGKA.TTF"
-exportpath = "./charimages/"
+fontpath = "../data/ttf/STXINGKA.TTF"
+exportpath = "../data/charimages/"
 size = 40
 
 start,end = (0x4E00, 0x9FA5) # unicode Chinese chars
 font = ImageFont.truetype(fontpath, size)
 
 # # a char not included in test font
-# text0 = unichr(0x5209)
+# text0 = chr(0x5209)
 # im0 = Image.new("L", (size, size), 255)
 # dr0 = ImageDraw.Draw(im0)
 # dr0.text((0, 0), text0, font=font, fill=0)
 
 for idx in range(start, end):
-    textu = unichr(idx)
+    textu = chr(idx)
     im = Image.new("L", (size, size), 255)
     dr = ImageDraw.Draw(im)
     dr.text((0, 0), textu, font=font, fill=0)
