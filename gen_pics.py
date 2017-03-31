9a1ecbb3045d6a83da28288c234db6698d77ef1d
Runjie Zhang <runjiehoos@gmail.com>
2017-03-30 17:21:04 -0700

    Improve render performance, add gen_pics

--- gen_pics.py
+++ gen_pics.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from PIL import Image, ImageFont, ImageDraw
 from fontTools.ttLib import TTFont
 
@@ -7,7 +8,7 @@ import re
 
 ttfpath = "../data/ttf/"
 fontsuffix = ".TTF"
-outpath = "../data/binary/"
+imgpath = "../data/charimages/"
 
 fonts = ["fangsong", "Kaiti", "MicrosoftYahei", "SimHei", "SimSun", "STHUPO", "STLITI", "STXINGKA", "STXINWEI","STZHONGS"]
 #fonts = ["fangsong"]
@@ -21,10 +22,12 @@ upper_bound = 0x9FA5
 
 for f in fonts:
     fontpath = ttfpath + f + fontsuffix
-    font = ImageFont.truetype(fontpath, font_size)
-    ttf = TTFont(fontpath, 0, allowVID=0, ignoreDecompileErrors=True, fontNumber=-1)
+    exportpath = imgpath + f + "/"
+    if not os.path.exists(exportpath):
+        os.makedirs(exportpath)
 
     char_list = []
+    ttf = TTFont(fontpath, 0, allowVID=0, ignoreDecompileErrors=True, fontNumber=-1)
     for x in ttf["cmap"].tables:
         for y in x.cmap.items():
             m = re.search('^uni([ABCDEF\d]+)$', y[1])
@@ -34,17 +37,12 @@ for f in fonts:
                 if hex_int >= lower_bound and hex_int <= upper_bound:
                     char_list.append(hex_int)    
 
-    data_array = np.empty([len(char_list), pic_size*pic_size])
-    i = 0
+    font = ImageFont.truetype(fontpath, font_size)
     for idx in char_list:
         textu = chr(idx)
         im = Image.new("L", (pic_size, pic_size), 255)
         dr = ImageDraw.Draw(im)
         dr.text((0, 0), textu, font=font, fill=0)
-        data_array[i,:] = np.array(im).reshape(1, 28*28)
-        i = i + 1
-    
-    binary_file = outpath + f
-    np.save(binary_file, data_array)
+        im.save(exportpath + textu + ".png")
     
     print('Done with font \"{0}\", found {1} valid chars.'.format(f, len(char_list)))
